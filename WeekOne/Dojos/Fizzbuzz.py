dbcc9679ad3f39307ae01a1be06425fe840af81d
Wole Solana <osolana@thoughtworks.com>
2015-04-05 23:19:52 +0200

    add primer for fizzbuzz

---
+++ WeekOne/Dojos/Fizzbuzz.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+
+class Fizzbuzz(object):
+    def convert(self, number):
+        return "Fizz"
---
+++ WeekOne/Dojos/test_fizzbuzz.py
@@ -0,0 +1,7 @@
+import unittest
+from unittest import TestCase
+from Fizzbuzz import *
+
+class TestFizzbuzz(TestCase):
+    def test_should_return_fizz_if_the_number_is_divisible_by_3(self):
+        self.assertEqual(Fizzbuzz().convert(3), "Fizz")
