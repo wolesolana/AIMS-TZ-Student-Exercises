29df1bb7dc7367077e7e34b5961404c44677701c
Wole Solana <osolana@thoughtworks.com>
2015-04-06 13:37:49 +0200

    skeletal structure for roman numerals kata

---
+++ WeekOne/Dojos/RomanNumerals.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+
+class RomanNumerals(object):
+    pass
---
+++ WeekOne/Dojos/test_romanNumerals.py
@@ -0,0 +1,8 @@
+import unittest
+from unittest import TestCase
+
+class TestRomanNumerals(TestCase):
+    pass
+
+if __name__ == '__main__':
+    unittest.main()
