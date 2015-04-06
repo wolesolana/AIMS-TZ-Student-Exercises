0a867970b7b132b25d69df55f43803472a178ed0
Wole Solana <osolana@thoughtworks.com>
2015-04-06 09:59:38 +0200

    allow tests to be run from command line

--- WeekOne/Dojos/test_fizzbuzz.py
+++ WeekOne/Dojos/test_fizzbuzz.py
@@ -5,3 +5,7 @@ from Fizzbuzz import *
 class TestFizzbuzz(TestCase):
     def test_should_return_fizz_if_the_number_is_divisible_by_3(self):
         self.assertEqual(Fizzbuzz().convert(3), "Fizz")
+
+
+if __name__ == '__main__':
+    unittest.main()
