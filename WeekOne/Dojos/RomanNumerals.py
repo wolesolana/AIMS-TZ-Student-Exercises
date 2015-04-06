202f79339a4ac72d26e3de8d2e091321b2620b4a
Wole Solana <osolana@thoughtworks.com>
2015-04-06 15:00:38 +0200

    add solution pattern for roman numeral kata

--- WeekOne/Dojos/RomanNumerals.py
+++ WeekOne/Dojos/RomanNumerals.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python
+from enum import Enum
+
+class BaseRomanNumerals(Enum):
+    X = 10
+    IX = 9
+    V = 5
+    IV = 4
+    I = 1
 
 class RomanNumerals(object):
-    pass
+    def convert_to_roman(self, arabic_number):
+        converted_number = ""
+
+        for numeral in BaseRomanNumerals.__reversed__():
+            while arabic_number >= numeral.value:
+                converted_number += numeral.name
+                arabic_number -= numeral.value
+
+        return converted_number
--- WeekOne/Dojos/test_romanNumerals.py
+++ WeekOne/Dojos/test_romanNumerals.py
@@ -1,8 +1,28 @@
 import unittest
 from unittest import TestCase
+from RomanNumerals import *
 
 class TestRomanNumerals(TestCase):
-    pass
+    def test_converts_1_to_I(self):
+        self.assertEqual(RomanNumerals().convert_to_roman(1), "I")
+
+    def test_converts_2_to_II(self):
+        self.assertEqual(RomanNumerals().convert_to_roman(2), "II")
+
+    def test_converts_4_to_IV(self):
+        self.assertEqual(RomanNumerals().convert_to_roman(4), "IV")
+
+    def test_converts_5_to_V(self):
+        self.assertEqual(RomanNumerals().convert_to_roman(5), "V")
+
+    def test_converts_6_to_VI(self):
+        self.assertEqual(RomanNumerals().convert_to_roman(6), "VI")
+
+    def test_converts_9_to_IX(self):
+        self.assertEqual(RomanNumerals().convert_to_roman(9), "IX")
+
+    def test_converts_10_to_X(self):
+        self.assertEqual(RomanNumerals().convert_to_roman(10), "X")
 
 if __name__ == '__main__':
     unittest.main()
